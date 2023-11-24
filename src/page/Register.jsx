3f5a978f25cd410842e6addab2cf1798a1d6dabb
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-11-25 01:24:09 +0700

    make page user registrasi

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,7 +1,133 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import { mainlogo } from "../assets";
+import { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline";
 
-export default Register
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
+      {/* Left Section */}
+      <div className="p-10 flex items-center justify-center ml-16 mx-9 bg-white">
+        <form className="w-full lg:w-80 flex flex-col items-start">
+          <h2 className="text-3xl font-bold mb-6 text-indigo-600 self-start">
+            Daftar
+          </h2>
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
+              style={{
+                borderRadius: "16px",
+              }}
+              placeholder="Nama Lengkap"
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
+              style={{
+                borderRadius: "16px",
+              }}
+              placeholder="Contoh: johndoe@gmail.com"
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="phoneNumber"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Nomor Telepon
+            </label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
+              style={{
+                borderRadius: "16px",
+              }}
+              placeholder="+62."
+            />
+          </div>
+
+          <div className="mb-4 w-full">
+            <div className="flex justify-between items-center mb-2 w-full">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Buat Password
+              </label>
+            </div>
+            <div className="relative w-full">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                className="mt-1 p-3 w-full border rounded-md pr-10 pl-3"
+                style={{
+                  borderRadius: "16px",
+                }}
+                placeholder="Buat Password"
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <EyeOffIcon className="h-6 w-6 text-gray-400" />
+                ) : (
+                  <EyeIcon className="h-6 w-6 text-gray-400" />
+                )}
+              </button>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2 px-4 mb-5 bg-indigo-600 text-white rounded hover:bg-indigo-600"
+            style={{ borderRadius: "16px" }}
+          >
+            Daftar
+          </button>
+
+          <p className="mt-4 text-gray-600 flex items-center justify-center w-full">
+            Belum punya akun ?&nbsp;
+            <a href="/login" className="text-indigo-600">
+              Masuk disini
+            </a>
+          </p>
+        </form>
+      </div>
+
+      {/* Right Section */}
+      <div className="hidden ms-24 lg:flex bg-[#6148FF] items-center justify-center text-white">
+        <img src={mainlogo} alt="Logo" className="text-3xl font-semibold" />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
