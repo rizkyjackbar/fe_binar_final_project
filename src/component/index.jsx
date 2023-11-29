0bc77e3ffef740e4c5ba241884d8bfcf805f762a
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-11-29 10:13:25 +0700

    add new path :rizz

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,7 +1,8 @@
 import Button from "./Button";
 import MainNavbar from "./MainNavbar";
+import Navbar from "./Navbar";
 import CardCategory from "./cardCategory";
 import CardCourse from "./CardCourse";
 import ButtonBuy from "./ButtonBuy";
 
-export { Button, MainNavbar, CardCategory, CardCourse, ButtonBuy };
+export { Button, MainNavbar, Navbar, CardCategory, CardCourse, ButtonBuy };
