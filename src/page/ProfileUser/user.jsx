3628bb4ea19a1708246875a7d62a313a6dc6e7a3
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-11-29 15:16:15 +0700

    page notification user :rizzz

--- src/page/ProfileUser/user.jsx
+++ src/page/ProfileUser/user.jsx
@@ -1,10 +1,9 @@
+
 import { Navbar } from "../../component";
-import { BellIcon, ArrowLeftIcon } from "@heroicons/react/solid";
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+import IsiCard from "../../component/InNotifCard";
 
 const NotificationUser = () => {
-  const isUnread = true;
-  const isRead = false;
-
   return (
     <div className="">
       <header>
@@ -21,75 +20,27 @@ const NotificationUser = () => {
 
         <div className="flex items-center justify-center">
           <div className="card bg-white shadow-md mt-12 w-2/3 h-auto border border-indigo-600 rounded-2xl">
-            <h2 className="bg-indigo-600 flex items-center justify-center text-sm font-bold text-white rounded-t-2xl mb-4 h-12">
+            <h2 className="bg-indigo-600 flex items-center justify-center text-sm font-bold text-white rounded-t-2xl h-12">
               Notifikasi
             </h2>
-            <div className="isiCard p-4 flex items-center">
-              <div className="bg-indigo-600 p-1 rounded-full">
-                <BellIcon className="w-6 h-6 text-white" />
-              </div>
-              <div className="ml-4">
-                <h2 className="text-indigo-600">Promosi</h2>
-                <p className="text-gray-800 font-bold">
-                  Dapatkan Potongan 50% selama Bulan Maret!
-                </p>
-                <p className="text-sm text-gray-500">
-                  Syarat dan Ketentuan berlaku!
-                </p>
-              </div>
-              <div className="ml-auto text-right flex items-center">
-                <p className="text-sm text-gray-500">2 Maret, 12:00</p>
-                <div
-                  className={`ml-2 w-4 h-4 rounded-full ${
-                    isUnread ? "bg-red-500" : "bg-green-500"
-                  }`}
-                ></div>
-              </div>
-            </div>
 
-            <div className="isiCard p-4 flex items-center">
-              <div className="bg-indigo-600 p-1 rounded-full">
-                <BellIcon className="w-6 h-6 text-white" />
-              </div>
-              <div className="ml-4">
-                <h2 className="text-indigo-600">Notifikasi</h2>
-                <p className="text-gray-800 font-bold">
-                  Password berhasil diubah
-                </p>
-                <p className="text-sm text-gray-500"></p>
-              </div>
-              <div className="ml-auto text-right flex items-center">
-                <p className="text-sm text-gray-500">1 Maret, 10:00</p>
-                <div
-                  className={`ml-2 w-4 h-4 rounded-full ${
-                    isRead ? "bg-red-500" : "bg-green-500"
-                  }`}
-                ></div>
-              </div>
-            </div>
+            <IsiCard
+              title="Promosi"
+              content="Dapatkan Potongan 50% selama Bulan Maret!"
+              date="2 Maret, 12:00"
+            />
 
-            <div className="isiCard p-4 flex items-center">
-              <div className="bg-indigo-600 p-1 rounded-full">
-                <BellIcon className="w-6 h-6 text-white" />
-              </div>
-              <div className="ml-4">
-                <h2 className="text-indigo-600">Promosi</h2>
-                <p className="text-gray-800 font-bold">
-                  Dapatkan Potongan 50% selama Bulan Maret!
-                </p>
-                <p className="text-sm text-gray-500">
-                  Syarat dan Ketentuan berlaku!
-                </p>
-              </div>
-              <div className="ml-auto text-right flex items-center">
-                <p className="text-sm text-gray-500">1 Maret, 09:00</p>
-                <div
-                  className={`ml-2 w-4 h-4 rounded-full ${
-                    isUnread ? "bg-red-500" : "bg-green-500"
-                  }`}
-                ></div>
-              </div>
-            </div>
+            <IsiCard
+              title="Notifikasi"
+              content="Password berhasil diubah"
+              date="1 Maret, 10:00"
+            />
+
+            <IsiCard
+              title="Promosi"
+              content="Dapatkan Potongan 50% selama Bulan Maret!"
+              date="1 Maret, 09:00"
+            />
           </div>
         </div>
       </div>
