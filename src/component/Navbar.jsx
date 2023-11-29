e4defa45762dbd2a95e8fd21f0a70c0a5658ac0a
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-11-29 09:55:14 +0700

    rapihin searchbar :rizzz

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Cari Kursus terbaik...."
-              className="w-full h-full text-gray-900 "
+              className="w-full h-full text-gray-900 outline-none focus:outline-none"
             />
             <button className="flex items-center justify-center w-[2.375rem] h-[2.375rem] bg-[#6148FF] rounded-[0.625rem]">
-              <img src={bx_search} />
+              <img src={bx_search} alt="Search Icon" />
             </button>
           </div>
         </div>
