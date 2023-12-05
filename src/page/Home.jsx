c9506f2d77955895b1d44641979df98630fb9d3e
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-12-05 18:46:53 +0700

    benerin margin antar  card pada landingpage

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -17,49 +17,49 @@ const Home = () => {
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "All",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "Data Science",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "Android Development",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "Web Development",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "IOS Development",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "Business Intelligence",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
     {
       colorAf: "#6148FF",
       colorBf: "#E8F1FF",
       textColor: "black",
       text: "UI/UX Design",
-      classes: "rounded-2xl px-5 py-1",
+      classes: "rounded-2xl px-3",
     },
   ];
 
@@ -97,23 +97,44 @@ const Home = () => {
                 Lihat Semua
               </a>
             </div>
-            <div></div>
-            <div className="p-5">
-              <div className="flex flex-row justify-between">
-                <CardCategory link={"#"} img={UIUX} label={"UI/UX Design"} />
+            <div className="p-5 font-semibold">
+              <div className="flex flex-row justify-between m">
+                <CardCategory
+                  link={"#"}
+                  img={UIUX}
+                  label={"UI/UX Design"}
+                  className="px-10"
+                />
                 <CardCategory
                   link={"#"}
                   img={PM}
                   label={"Product Management"}
+                  className="px-10"
                 />
-                <CardCategory link={"#"} img={WEB} label={"Web Developer"} />
+                <CardCategory
+                  link={"#"}
+                  img={WEB}
+                  label={"Web Developer"}
+                  className="px-10"
+                />
                 <CardCategory
                   link={"#"}
                   img={AND}
                   label={"Android Development"}
+                  className="px-10"
                 />
-                <CardCategory link={"#"} img={IOS} label={"IOS Development"} />
-                <CardCategory link={"#"} img={DS} label={"Data Science"} />
+                <CardCategory
+                  link={"#"}
+                  img={IOS}
+                  label={"IOS Development"}
+                  className="px-10"
+                />
+                <CardCategory
+                  link={"#"}
+                  img={DS}
+                  label={"Data Science"}
+                  className="px-10"
+                />
               </div>
             </div>
           </div>
@@ -127,7 +148,7 @@ const Home = () => {
                 Lihat Semua
               </a>
             </div>
-            <div className="flex mt-3 px-6 justify-between text-sm font-bold">
+            <div className="flex mt-3 justify-between text-sm font-bold">
               {buttonData.map((button, index) => (
                 <Button
                   key={index}
