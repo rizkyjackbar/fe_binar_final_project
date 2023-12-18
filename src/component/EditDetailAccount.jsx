30477a71020018c8889ff5dbd0b46b79dec9de57
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-12-19 00:20:00 +0700

    integrasi update profile

--- src/component/EditDetailAccount.jsx
+++ src/component/EditDetailAccount.jsx
@@ -1,126 +1,298 @@
-import { DummyProfile } from "../assets";
+import { useState, useEffect } from "react";
+// import { DummyProfile } from "../assets";
 
 const EditDetailAccount = () => {
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setphone_number] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [alert, setAlert] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userDataString = localStorage.getItem("userData");
+        if (userDataString) {
+          const userData = JSON.parse(userDataString);
+          setPhoto(userData.photo || "");
+          setName(userData.name || "");
+          setEmail(userData.email || "");
+          setphone_number(userData.phone_number || "");
+          setCountry(userData.country || "");
+          setCity(userData.city || "");
+        } else {
+          setPhoto("");
+        }
+
+        const response = await fetch(
+          "https://befinalprojectbinar-production.up.railway.app/api/user",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const userData = await response.json();
+          setPhoto(userData.photo || "");
+          setName(userData.name || "");
+          setEmail(userData.email || "");
+          setphone_number(userData.phone_number || "");
+          setCountry(userData.country || "");
+          setCity(userData.city || "");
+          console.log(userData);
+        } else {
+          console.error("Gagal mengambil data pengguna");
+        }
+      } catch (error) {
+        console.error("Kesalahan selama pengambilan data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const storedPhoto = localStorage.getItem("userPhoto");
+    if (storedPhoto) {
+      setPhoto(storedPhoto);
+    }
+  }, []);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPhoto(reader.result);
+        localStorage.setItem("userPhoto", reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://befinalprojectbinar-production.up.railway.app/api/user",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            phone_number,
+            country,
+            city,
+            photo,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const userData = {
+          name,
+          email,
+          phone_number,
+          country,
+          city,
+          photo,
+        };
+        localStorage.setItem("userData", JSON.stringify(userData));
+
+        setAlert({
+          type: "success",
+          message: "Profil berhasil diperbarui!",
+        });
+
+        console.log("Profil berhasil diperbarui!", userData);
+        setTimeout(() => {
+          setAlert(null);
+        }, 3000);
+      } else {
+        setAlert({
+          type: "error",
+          message: "Gagal memperbarui profil",
+        });
+        console.error("Gagal memperbarui profil");
+        setTimeout(() => {
+          setAlert(null);
+        }, 3000);
+      }
+    } catch (error) {
+      setAlert({
+        type: "error",
+        message: "Kesalahan selama pembaruan profil",
+      });
+      console.error("Kesalahan selama pembaruan profil:", error);
+    }
+  };
+
   return (
     <div className="EditDetailAccount p-4">
-      <div className="mb-4 text-center">
-        <img
-          src={DummyProfile}
-          alt=""
-          className="w-24 h-24 object-cover rounded-full mx-auto"
-        />
-      </div>
-
-      <div className="mb-4 w-full">
-        <label
-          htmlFor="name"
-          className="block text-sm font-medium text-gray-600"
-        >
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
-          style={{
-            borderRadius: "16px",
-          }}
-          placeholder="Nama Lengkap"
-        />
-      </div>
-
-      <div className="mb-4 w-full">
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-600"
-        >
-          Email
-        </label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
-          style={{
-            borderRadius: "16px",
-          }}
-          placeholder="Contoh: johndoe@gmail.com"
-        />
-      </div>
-
-      <div className="mb-4 w-full">
-        <label
-          htmlFor="phoneNumber"
-          className="block text-sm font-medium text-gray-600"
-        >
-          Nomor Telepon
-        </label>
-        <input
-          type="tel"
-          id="phoneNumber"
-          name="phoneNumber"
-          className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
-          style={{
-            borderRadius: "16px",
-          }}
-          placeholder="+62."
-        />
-      </div>
-
-      <div className="mb-4 w-full">
-        <div className="flex justify-between items-center w-full">
-          <label
-            htmlFor="country"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Negara
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4 text-center">
+          <label htmlFor="photo" className="cursor-pointer">
+            <input
+              type="file"
+              id="photo"
+              name="photo"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+            <img
+              src={photo}
+              alt=""
+              className="w-24 h-24 object-cover rounded-full mx-auto cursor-pointer"
+            />
           </label>
         </div>
-        <div className="relative w-full">
+
+        <div className="mb-4 w-full">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Nama
+          </label>
           <input
-            id="country"
             type="text"
-            name="country"
-            className="mt-1 p-3 w-full border rounded-md pr-10 pl-3"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
             style={{
               borderRadius: "16px",
             }}
-            placeholder="Masukkan Negara Tempat Tinggal"
+            placeholder="Nama Lengkap"
           />
         </div>
-      </div>
 
-      <div className="mb-4 w-full">
-        <div className="flex justify-between items-center w-full">
+        <div className="mb-4 w-full">
           <label
-            htmlFor="city"
+            htmlFor="email"
             className="block text-sm font-medium text-gray-600"
           >
-            Kota
+            Email
           </label>
-        </div>
-        <div className="relative w-full">
           <input
-            id="city"
             type="text"
-            name="city"
-            className="mt-1 p-3 w-full border rounded-md pr-10 pl-3"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
             style={{
               borderRadius: "16px",
             }}
-            placeholder="Masukkan Kota Tempat Tinggal"
+            placeholder="Contoh: johndoe@gmail.com"
           />
         </div>
-      </div>
 
-      <button
-        type="submit"
-        className="w-full py-2 px-4 mb-5 font-bold bg-indigo-600 text-white rounded hover:bg-indigo-600"
-        style={{ borderRadius: "16px" }}
-      >
-        Simpan Profil Saya
-      </button>
+        <div className="mb-4 w-full">
+          <label
+            htmlFor="phone_number"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Nomor Telepon
+          </label>
+          <input
+            type="tel"
+            id="phone_number"
+            name="phone_number"
+            value={phone_number}
+            onChange={(e) => setphone_number(e.target.value)}
+            className="mt-1 p-3 w-full border rounded-md pl-3 pr-3"
+            style={{
+              borderRadius: "16px",
+            }}
+            placeholder="+62."
+          />
+        </div>
+
+        <div className="mb-4 w-full">
+          <div className="flex justify-between items-center w-full">
+            <label
+              htmlFor="country"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Negara
+            </label>
+          </div>
+          <div className="relative w-full">
+            <input
+              id="country"
+              type="text"
+              name="country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              className="mt-1 p-3 w-full border rounded-md pr-10 pl-3"
+              style={{
+                borderRadius: "16px",
+              }}
+              placeholder="Masukkan Negara Tempat Tinggal"
+            />
+          </div>
+        </div>
+
+        <div className="mb-4 w-full">
+          <div className="flex justify-between items-center w-full">
+            <label
+              htmlFor="city"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Kota
+            </label>
+          </div>
+          <div className="relative w-full">
+            <input
+              id="city"
+              type="text"
+              name="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              className="mt-1 p-3 w-full border rounded-md pr-10 pl-3"
+              style={{
+                borderRadius: "16px",
+              }}
+              placeholder="Masukkan Kota Tempat Tinggal"
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 mb-5 font-bold bg-indigo-600 text-white rounded hover:bg-indigo-600"
+          style={{ borderRadius: "16px" }}
+        >
+          Simpan Profil Saya
+        </button>
+        <div className="flex items-center justify-center">
+          {alert && (
+            <div
+              className={`text-${
+                alert.type === "success" ? "green" : "red"
+              }-500 bg-${
+                alert.type === "success" ? "green" : "red"
+              }-100 p-2 rounded-xl`}
+            >
+              {alert.message}
+            </div>
+          )}
+        </div>
+      </form>
     </div>
   );
 };
