229b2f97f09ec5cc091924e42bfde575e69a46c8
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-12-28 21:24:23 +0700

    create riwayat transc :rizzz

--- src/component/PaymentHistory.jsx
+++ src/component/PaymentHistory.jsx
@@ -11,6 +11,7 @@ class PaymentHistory extends Component {
     super(props);
     this.state = {
       paymentHistory: [],
+      isLoading: true,
     };
   }
 
@@ -33,19 +34,20 @@ class PaymentHistory extends Component {
 
         if (data.status === "OK") {
           const paymentHistory = data.data;
-
-          this.setState({ paymentHistory });
+          this.setState({ paymentHistory, isLoading: false });
         } else {
           console.error("API request failed. Status:", data.status);
+          this.setState({ isLoading: false });
         }
       })
       .catch((error) => {
         console.error("Error fetching payment history:", error);
+        this.setState({ isLoading: false });
       });
   }
 
   render() {
-    const { paymentHistory } = this.state;
+    const { paymentHistory, isLoading } = this.state;
 
     return (
       <div className="PaymentHistory p-4 d-flex flex-column align-items-center">
@@ -54,7 +56,11 @@ class PaymentHistory extends Component {
         </div>
 
         <div className="d-flex flex-wrap justify-content-center">
-          {paymentHistory.length === 0 ? (
+          {isLoading ? (
+            <div className="text-center my-16 bg-gradient-to-r from-blue-500 to-purple-500 text-white p-2 mx-16 rounded-lg shadow-lg">
+              <p className="text-xl font-bold">Tunggu Sebentar....</p>
+            </div>
+          ) : paymentHistory.length === 0 ? (
             <div className="text-center my-16 bg-gradient-to-r from-blue-500 to-purple-500 text-white p-2 mx-16 rounded-lg shadow-lg">
               <p className="text-xl font-bold">Tidak Ada Pembayaran</p>
             </div>
@@ -66,17 +72,14 @@ class PaymentHistory extends Component {
                   idCourse={payment.course.id}
                   classCategory={payment.course.category.category}
                   classesName={payment.course.name}
-                  rating={payment.course.rating}
-                  classMentor={payment.course.mentor}
+                  classMentor={payment.course.facilitator}
                   level={payment.course.level}
-                  moduls={payment.course.moduls}
-                  times={payment.course.times}
+                  moduls={payment.course.total_chapter}
+                  times={payment.course.total_duration}
                 >
                   {payment.status === "SUDAH BAYAR" && <ButtonPaid />}
-                  {payment.status === "BELUM BAYAR" && <ButtonPaidFailed />}
-                  {payment.status === "MENUNGGU PEMBAYARAN" && (
-                    <ButtonPaidWaiting />
-                  )}
+                  {payment.status === "GAGAL BAYAR" && <ButtonPaidFailed />}
+                  {payment.status === "BELUM BAYAR" && <ButtonPaidWaiting />}
                 </CardCourse>
               </div>
             ))
