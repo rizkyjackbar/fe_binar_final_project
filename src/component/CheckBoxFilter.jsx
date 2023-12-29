b0a5a3247ca868b86e873ac6ed095829ac4c2e5a
Muhammad Rizky <muhamadrizky62020@students.amikom.ac.id>
2023-12-29 15:53:17 +0700

    yoi :rizz

--- src/component/CheckBoxFilter.jsx
+++ src/component/CheckBoxFilter.jsx
@@ -1,12 +1,7 @@
 const CheckBoxFilter = ({ isChecked, onChange, name }) => {
   return (
     <label className="flex items-center mb-2 cursor-pointer">
-      <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={onChange}
-        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-      />
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
       <span className="ms-2 text-xs font-normal text-gray-500">{name}</span>
     </label>
   );
