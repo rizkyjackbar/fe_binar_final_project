eae0a1685880d25ca106018a0c08266fb9a5774f
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-01-02 18:09:02 +0700

    ubah ukuran font card

--- src/component/CardCourse.jsx
+++ src/component/CardCourse.jsx
@@ -25,7 +25,7 @@ const CardCourse = ({
       <Link className="cardCourse" to={`/detailclass/${id}`}>
         <img src={img} className="rounded-t-2xl w-full h-20 object-cover" />
         <div className="cardBody px-2.5 py-2 flex flex-col gap-0.5">
-          <p className="text-[0.625rem] font-bold text-[#6148FF]">
+          <p className="text-[0.7rem] font-bold text-[#6148FF]">
             {classCategory}
           </p>
           <p className="text-[0.8rem] font-bold">{classesName}</p>
