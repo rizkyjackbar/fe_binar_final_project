5fbbf6fe7822eebaee8b4014b2f2395fca8ea8ad
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-01-02 19:07:42 +0700

    add crete tracker home

--- src/component/CardCategory.jsx
+++ src/component/CardCategory.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const CardCategory = ({ link, img, label, classes }) => {
   return (
     <span className={classes}>
-      <a
+      <Link
         href={link}
+        // state={}
         className="flex items-center flex-col gap-4 text-center mx-2"
       >
         <img
@@ -11,7 +14,7 @@ const CardCategory = ({ link, img, label, classes }) => {
           className=" w-[100px] lg:w-[8.75rem] h-[6.25rem] rounded-3xl object-cover"
         />
         <label className=" text-[12px] md:text-[16px]">{label}</label>
-      </a>
+      </Link>
     </span>
   );
 };
